70c436ecb0a2cd293536b8b75a54cdee4a7109d8
Takeda Hiroyuki <takeda.hiroyuki@gmail.com>
2011-06-28 13:58:25 +0900

    Excercise 2: Second Test

--- src/sample/WikiEngineImpl.php
+++ src/sample/WikiEngineImpl.php
@@ -3,7 +3,7 @@ require_once("WikiEngine.php");
 
 class WikiEngineImpl {
   function toHtml($input){
-    return "Hello World";
+    return $input;
   }
 }
 ?>
--- test/sample/WikiEngineImplTest.php
+++ test/sample/WikiEngineImplTest.php
@@ -3,14 +3,27 @@ require_once("/src/sample/WikiEngineImpl.php");
 
 class WikiEngineImplTest extends PHPUnit_Framework_TestCase {
   
+  public function setUp(){
+    $this->target = new WikiEngineImpl();
+  }
+  
   /**
    * @test
    */
   public function toHtml_HelloWorld() {
-    $target = new WikiEngineImpl();
     $input = "Hello World";
     $expected = "Hello World";
-    $actual = $target->toHtml($input);
+    $actual = $this->target->toHtml($input);
+    $this->assertEquals($actual,$expected);
+  }
+  
+  /**
+   * @test
+   */
+  public function toHtml_TDD_Bootcamp() {
+    $input = "TDD Bootcamp";
+    $expected = "TDD Bootcamp";
+    $actual = $this->target->toHtml($input);
     $this->assertEquals($actual,$expected);
   }
 
